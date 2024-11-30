a26ec9bf2fce782c2e1fa1764843aeef27bd9d0a
andre-0303 <bandeiradev.ti@gmail.com>
2024-11-30 20:53:05 -0300

    primeiro-projeto-react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Meu primeiro programa com React e Vite!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+      <button id="botao">Clique aqui</button>
+    </div>
+  )
+
+}
+
+export default App
